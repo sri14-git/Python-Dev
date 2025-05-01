2292a148d53957acd0a3dcd1921f831fded1e721
sri14-git <srivarshan14@gmail.com>
2025-05-01 19:15:47 +0530

    File Handling(csv and txt)

--- FileHandling/main.py
+++ FileHandling/main.py
@@ -1,4 +1,5 @@
 # This is a sample Python script.
+import time
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -6,7 +7,14 @@
 
 
 file=open("hello.txt",'w')
-file.writelines("Hello world")
+file.write("Hello worl")
+file.close()
 # Press the green button in the gutter to run the script.
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a=[1,2,3,4]
+b=[1,2,3]
+print(list(zip(a,b)))
+# print(help(time))
+print(time.strftime("%Y"))
+print(ord("A"))
