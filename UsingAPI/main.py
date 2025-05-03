f11325c671b77b1180b12356885fd64ea00187e8
sri14-git <srivarshan14@gmail.com>
2025-05-03 22:39:05 +0530

    API request Demo

--- UsingAPI/main.py
+++ UsingAPI/main.py
@@ -1,7 +1,9 @@
 import requests as rq
 api_key="ebbc07135b2247dfbf847efbcdf29df0"
 url="https://newsapi.org/v2/everything?q=tesla&from=2025-04-03" \
-    "&sortBy=publishedAt&apiKey=ebbc07135b2247dfbf847efbcdf29df0"
+    "&sortBy=publishedAt&apiKey=ebbc07135b2247dfbf847efbcdf29df0&language=en"
 request=rq.get(url)
-content=request.text
-print(content)
+content=request.json()
+for item in content["articles"]:
+    print(item["title"])
+    print(item["description"])
